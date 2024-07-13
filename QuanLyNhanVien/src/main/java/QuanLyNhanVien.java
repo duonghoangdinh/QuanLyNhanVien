2cd6acf00904c17367d75df2da220f9f9a054224
duonghoangdinh <dinhdhpc08382@gmail.com>
2024-07-13 10:34:35 +0700

    QuanLyNhanVien

---
+++ QuanLyNhanVien/src/main/java/QuanLyNhanVien.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class QuanLyNhanVien {
+
+    public static void main(String[] args) {
+        //hello
+        //hi
+    }
+}
